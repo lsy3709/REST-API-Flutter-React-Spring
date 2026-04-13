867ecb4ce8b7b9f9aae7410480830684c7e2aca7
lsy3709 <lsy3709@naver.com>
2026-04-13 09:49:54 +0900

    first commit

---
+++ src/main/java/com/busanit401/restapibootflutterreact/RestApiBootFlutterReactApplication.java
@@ -0,0 +1,13 @@
+package com.busanit401.restapibootflutterreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBootFlutterReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiBootFlutterReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/busanit401/restapibootflutterreact/RestApiBootFlutterReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit401.restapibootflutterreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBootFlutterReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
